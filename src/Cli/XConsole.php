4dfae3b3962aecd7bfb30d829d4f247bf5f81a08
李铭昕 <715557344@qq.com>
2017-09-09 20:29:51 +0800

    * 完善基本的功能

--- src/Cli/XConsole.php
+++ src/Cli/XConsole.php
@@ -9,11 +9,59 @@
 namespace Xin\Phalcon\Cli;
 
 use Phalcon\Cli\Console;
+use Phalcon\Text;
 
 class XConsole extends Console
 {
-    public function handle(array $arguments = null)
+    const ARGUMENTS_TYPE_ARRAY = 0;
+    const ARGUMENTS_TYPE_STRING = 1;
+
+    protected $argvType;
+
+    public function handle(array $argv = null)
     {
+        $arguments = $this->getArgumentsFromArgv($argv);
         return parent::handle($arguments);
     }
+
+    public function getArgumentsFromArgv(array $argv = null)
+    {
+        $task = $argv[1];
+        $arguments = [];
+        if (strpos($task, ':') !== false || strpos($task, '@') !== false) {
+            // 新Cli入参方式
+            list($task, $action) = explode('@', $argv[1]);
+            if (empty($action)) {
+                $action = 'main';
+            }
+            if (empty($task)) {
+                $task = 'Main';
+            } else {
+                $task = implode('\\', array_map(
+                    function ($v) {
+                        return Text::camelize($v);
+                    },
+                    explode(':', $task)
+                ));
+            }
+            $arguments['task'] = $task;
+            $arguments['action'] = $action;
+            if (isset($argv[2])) {
+                $arguments['params'] = array_slice($argv, 2, count($argv) - 2);
+            }
+        } else {
+            foreach ($argv as $k => $arg) {
+                if ($k == 1) {
+                    $arguments['task'] = $arg;
+                } elseif ($k == 2) {
+                    $arguments['action'] = $arg;
+                } elseif ($k >= 3) {
+                    $arguments['params'][] = $arg;
+                }
+            }
+        }
+        return $arguments;
+    }
+
+
 }
